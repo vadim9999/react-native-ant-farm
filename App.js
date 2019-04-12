980c9dac79ffb7315158eceaf2b54061ff94db2a
Vadim <vadim.putrov@gmail.com>
2019-04-13 00:19:15 +0300

    clean up

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { StyleSheet, Text, View, Button, AppRegistry, TextInput, CheckBox } from 'react-native';
 import EasyBluetooth from 'easy-bluetooth-classic';
 import Scan from "./components/Scan"
+import { setListener, init, onStartScan } from "./selector/selector"
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -19,30 +21,17 @@ export default class App extends React.Component {
 
     this.onPress = this.onPress.bind(this)
     this.onConnect = this.onConnect.bind(this)
-    this.onStartScan = this.onStartScan.bind(this)
+    this.getDevice = this.getDevice.bind(this)
+    // this.onStartScan = this.onStartScan.bind(this)
   }
 
-  config = {
-    "uuid":
-    "00001101-0000-1000-8000-00805f9b34fb",
-    "deviceName": "Samsung",
-    "bufferSize": 1024,
-    "characterDelimiter": "\n"
-  }
-
-  init() {
-    EasyBluetooth.init(this.config)
-      .then(function (config) {
-        console.log("config done!");
-      })
-      .catch(function (ex) {
-        console.warn(ex);
-      });
-  }
   
-  componentWillMount() {
-    this.init();
-    this.onStartScan()
+  
+ 
+
+    
+    
+    
     // EasyBluetooth.startScan()
     //   .then(function (devices) {
     //     console.log("all devices found:");
@@ -54,45 +43,8 @@ export default class App extends React.Component {
     //   .catch(function (ex) {
     //     console.warn(ex);
     //   });
-    this.onDeviceFoundEvent = EasyBluetooth.addOnDeviceFoundListener(this.onDeviceFound.bind(this));
-    this.onStatusChangeEvent = EasyBluetooth.addOnStatusChangeListener(this.onStatusChange.bind(this));
-    this.onDataReadEvent = EasyBluetooth.addOnDataReadListener(this.onDataRead.bind(this));
-    this.onDeviceNameEvent = EasyBluetooth.addOnDeviceNameListener(this.onDeviceName.bind(this));
-  }
-  onDeviceFound(device) {
-    console.log("onDeviceFound");
-    console.log(device);
     
-    if (device.name == "raspberrypi") {
-      this.setState({
-        "device": device
-      })
-    }
-  }
-
-  onDataRead(data) {
-    console.log("onDataRead");
-    if ("OK" == data) {
-      console.log("Data is received");
-    }
-    console.log(data);
-  }
-
-  onDeviceName(name) {
-    console.log("onDeviceName");
-    console.log(name);
-  }
-  componentWillUnmount() {
-    this.onDeviceFoundEvent.remove();
-    this.onStatusChangeEvent.remove();
-    this.onDataReadEvent.remove();
-    this.onDeviceNameEvent.remove();
-  }
-
-  onStatusChange(status) {
-    console.log("onStatusChange");
-    console.log(status);
-  }
+  
   onPress() {
     this.onConnect()
     EasyBluetooth.writeln("Works in React Native!")
@@ -108,8 +60,9 @@ export default class App extends React.Component {
       connection: "З'єднання встановлено'"
     })
   }
-  getDevice() {
-    return this.state.device
+  getDevice(device) {
+    console.log(device);
+    
   }
   onConnect() {
     var device = this.getDevice()
@@ -126,47 +79,16 @@ export default class App extends React.Component {
       })
   }
 
-  onStartScan(){
-    console.log("Started scanning");
-    if(this.state.activity != true){
-    this.setState({
-      activity: true
-    })}
-    EasyBluetooth.startScan()
-      .then(function (devices) {
-        console.log("all devices found:");
-        // Todo add destroy dublicates
-        // console.log(devices);
-        var obj = {};
+  
 
-        for (var i = 0, len = devices.length; i < len; i++)
-          obj[devices[i]['address']] = devices[i];
-
-        result = new Array();
-        for (var address in obj)
-          result.push(obj[address]);
-        
-        
-        console.log(result);
-        
-        this.setState({
-          devices: result,
-          activity: false
-        })
-        console.log(result);
-      }.bind(this))
-      .catch(function (ex) {
-        console.warn(ex);
-      });
-  }
-
+  
   render() {
     console.log(this.state);
     
     return (
       <View style={styles.container}>
       <View style={styles.content}>
-      <Scan activity = {this.state.activity} startScan = {this.onStartScan} devices = {this.state.devices} />
+      <Scan getDevice = {this.getDevice} />
       <View style={styles.wifi_form}>
       <TextInput
         
--- components/Scan.js
+++ components/Scan.js
@@ -1,40 +1,90 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, Picker, ActivityIndicator } from 'react-native';
+import { setListener, init, onStartScan, getThis, bindListeners } from "../selector/selector"
+import EasyBluetooth from 'easy-bluetooth-classic';
 
 export default class Scan extends React.Component {
     constructor(props){
         super(props)
         this.state = {
             device: "",
-            // activity: true
+            activity: true,
+            devices:[]
             
           }
         this.onScan = this.onScan.bind(this)
+    }
+  
+    componentWillMount() {
+      init();
+      this.onScan()
+    // bindListeners(this)
+    // this.onDeviceFoundEvent = EasyBluetooth.addOnDeviceFoundListener(this.onDeviceFound.bind(this));
+    this.onDataReadEvent = EasyBluetooth.addOnDataReadListener(this.onDataRead.bind(this));
+    }
+
+  
+  onDataRead(data) {
+      console.log("onDataRead");
+      if ("OK" == data) {
+        console.log("Data is received");
+      }
+      console.log(data);
     }
     onScan(){
         // this.setState({
         //     activity:true
         // })
-        this.props.startScan()
+        
+        if(this.state.activity != true){
+          this.setState({
+            activity: true
+          })}
+
+        onStartScan().then(function (result){
+          console.log("in promise");
+          
+          console.log(result);
+          this.setState({
+          devices: result,
+          activity: false
+        })
+        }.bind(this)
+        )
+        .catch(function (ex) {
+          console.warn(ex);
+        });
+
+       
       }
+
+      componentWillUnmount() {
+        // this.onDeviceFoundEvent.remove();
+        // this.onStatusChangeEvent.remove();
+        this.onDataReadEvent.remove();
+        // this.onDeviceNameEvent.remove();
+      }
+    
     getListOfDevices(devices){
         console.log("getListOfDevices");
         // this.setState({
         //     activity: false
         // })
         if (devices != undefined ){
-            
+            if (devices.length == 1)
+              this.props.getDevice(devices[0])
             return (
-              
-                devices.map(device =>{ 
+
+                devices.map(device =>{
+                   
                   var pair = "";
                   if (device.uuids != undefined & device["uuids"].length > 0){
                     pair = "Пара"
                   }
                     if (device!= undefined & device.name !=undefined)
-                return <Picker.Item label={device.name + " " + device.address + " " + pair} value={device.name} />
+                return <Picker.Item label={device.name + " " + device.address + " " + pair} value={device.address} />
                 else if( device!= undefined & device.address != undefined){
-                    return <Picker.Item label={"NoName" + " " + device.address} value={"NoName"} />
+                    return <Picker.Item label={"NoName" + " " + device.address} value={device.address} />
                 } else return <Picker.Item label={""} value={""} />
                 }
             ))
@@ -54,13 +104,18 @@ export default class Scan extends React.Component {
             style={{ height: 50, width: 100 }}
             onValueChange={(itemValue, itemIndex) =>{
               console.log("Changed values");
-              
+              var device = this.state.devices.find( device =>{
+                return device.address == itemValue
+              })
+
+              this.props.getDevice(device)
+
               this.setState({ device: itemValue })}
             }>
-            {this.getListOfDevices(this.props.devices)}
+            {this.getListOfDevices(this.state.devices)}
             
           </Picker>
-          <ActivityIndicator animating={this.props.activity} size="small" color="#0000ff" />
+          <ActivityIndicator animating={this.state.activity} size="small" color="#0000ff" />
           </View>
         <View style={styles.buttons}>
           <Button
